14e0812d2cecc09a6ab7887ed40cdd5ab71974cb
Sayantan Banerjee <117575659+Creative-sayantan@users.noreply.github.com>
2022-11-16 12:15:07 +0530

    Add files via upload

---
+++ Sum&Avg_10num.py
@@ -0,0 +1,8 @@
+#Calculate the  sum & average of first 10 numbers
+n = int(input("Enter number: "))
+sum = 0
+for num in range(1, n + 1, 1):
+    sum = sum + num
+print("Sum of first ", n, "numbers is: ", sum)
+average = sum / n
+print("Average of ", n, "numbers is: ", average)
