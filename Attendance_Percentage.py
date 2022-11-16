8cad6bbc7003e77d31007a415eb9a74d8740bdf9
Sayantan Banerjee <117575659+Creative-sayantan@users.noreply.github.com>
2022-11-16 12:13:55 +0530

    Add files via upload

---
+++ Attendance_Percentage.py
@@ -0,0 +1,11 @@
+#Calculate percentage of how much class attend of total class
+total_class=int(input('Enter total classes: '))
+attended_classes=int(input('Enter the attended classes: '))
+
+avg_classes=attended_classes/total_class
+percentage_class = avg_classes*100
+
+print("Percentage of class = ")
+print(percentage_class)
+
+
